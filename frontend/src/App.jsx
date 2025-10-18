bd9f6897b050e40d8cc22b946f9136d948288a1a
NeilDMJ <dantemartinez175@gmail.com>
2025-10-18 17:43:36 -0600

    chore(docker) : replace nginx with default react server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +28,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <MyButton />
+      </div>
     </>
+  )
+}
+
+function MyButton() {
+  return (
+    <button>Boton</button>
   )
 }
 
