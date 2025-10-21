17aa26d7efb0a0b07ace2606a4b4f007eb818cf5
LGxnzalez <gogl030101@gs.utm.mx>
2025-10-20 21:24:28 -0600

    Actualizar main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,3 +15,42 @@ def root():
 @app.get("/api/sightings")
 def get_sightings():
     return list(db.sightings.find({}, {"_id": 0}))
+    
+    
+    
+def get_all_avistamientos():
+    pass
+
+def get_avistamiento_by_id(id: str):
+    pass
+
+def get_avistamientos_by_nombre_cientifico(nombre_cientifico: str):
+    pass
+
+def get_avistamientos_by_fecha(desde: str, hasta: str):
+    pass
+
+def get_avistamientos_by_pais(nombre_pais: str):
+    pass
+
+def get_avistamientos_by_taxonomia(
+    reino: str = None,
+    clase: str = None,
+    orden: str = None,
+    familia: str = None,
+    genero: str = None,
+    especie: str = None
+):
+    pass
+
+def get_avistamientos_by_ubicacion(lat: float, lng: float, radio_km: float):
+    pass
+
+def get_avistamientos_agrupados_por_pais():
+    pass
+
+def get_avistamientos_agrupados_por_fecha():
+    pass
+
+def get_avistamientos_agrupados_por_especie():
+    pass
