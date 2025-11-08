ebd6ca7921c2de973b42b0fbcc22e9322df3cdd4
NeilDMJ <dantemartinez175@gmail.com>
2025-11-08 15:11:08 -0600

    feat : dashborard y componentes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,22 @@
-
-import { Navbar as BSNavbar, Nav, NavDropdown, Form, Button, Container } from 'react-bootstrap';
+import { Navbar as BSNavbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     return(
         <BSNavbar expand="lg" className="custom-navbar">
             <Container fluid>
-                <BSNavbar.Brand href="/home">BioGeoVis</BSNavbar.Brand>
+                <BSNavbar.Brand as={Link} to="/home">
+                    BioGeoVis
+                </BSNavbar.Brand>
                 <BSNavbar.Toggle aria-controls="navbarSupportedContent" />
                 <BSNavbar.Collapse id="navbarSupportedContent">
                     <Nav className="me-auto mb-2 mb-lg-0">
-                        <Nav.Link href="/map" active>Mapa</Nav.Link>
-                        <Nav.Link href="/explorer">Explorar</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link as={Link} to="/home">Inicio</Nav.Link>
+                        <Nav.Link as={Link} to="/explorer">Explorador</Nav.Link>
+                        <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to="/about">Acerca de</Nav.Link>
                     </Nav>
-                    <Form className="d-flex" role="search">
-                        <Form.Control
-                            type="search"
-                            placeholder="Buscar coordenadas"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success" type="submit">Buscar</Button>
-                    </Form>
                 </BSNavbar.Collapse>
             </Container>
         </BSNavbar>
